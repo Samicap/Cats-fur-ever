696186c4e2ebfd5eeac5bc622742913bdbbbf6aa
samicap <lovestochasewindmills@gmail.com>
2021-04-20 22:25:55 -0400

    add token vaidation code

---
+++ cat-app/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+  // change to app.post later
+  res.send({
+    token: 'test123'
+  });
+});
+
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login')); 
--- cat-app/src/components/App/App.js
+++ cat-app/src/components/App/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';import Dashboard from '../Dashboard/Dashboard';
+import './App.css';
+import Login from '../Login/Login';
+// import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
 function App() {
+  const token = getToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
