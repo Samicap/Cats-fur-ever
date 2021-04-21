faa35436f1c9c79c2a4306fea592053d5164e7bb
samicap <lovestochasewindmills@gmail.com>
2021-04-21 16:51:25 -0400

    clean app.js and added cats components

--- cat-app/src/components/App/App.js
+++ cat-app/src/components/App/App.js
@@ -4,19 +4,12 @@ import './App.css';
 import Login from '../Login/Login';
 // import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import Cats from './Cats';
+import useToken from './useToken';
 
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
 
   if(!token) {
     return <Login setToken={setToken} />
@@ -32,6 +25,9 @@ function App() {
           </Route>
           <Route path="/preferences">
             <Preferences />
+          </Route>
+          <Route path="/cats">
+            <Cats />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ cat-app/src/components/Cats/Cats.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Cats() {
+  return(
+    <h2>Cats</h2>
+  );
+}
