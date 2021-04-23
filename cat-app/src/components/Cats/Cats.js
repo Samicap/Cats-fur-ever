54b4be7278e3cb8e599ad3997053b6e6e4686ef4
samicap <lovestochasewindmills@gmail.com>
2021-04-23 01:39:53 -0400

    add search button

--- cat-app/src/components/Cats/Cats.js
+++ cat-app/src/components/Cats/Cats.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+const axios = require('axios');
+
 export default function Cats() {
+
+  axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:1, size:"full"}})
+    .then((response) => {
+      console.log(response)
+    })
   return(
-    <h2>Cats</h2>
+    <div>
+      <h2>Cats</h2>
+      <button>Cat Search</button>
+
+    </div>
   );
 }
