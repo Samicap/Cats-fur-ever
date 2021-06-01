aa49143c90d1de282c056f6ec9bc3a94399e089e
samicap <lovestochasewindmills@gmail.com>
2021-06-01 17:22:54 -0400

    removed padding from form.js wrapper. the header will now extend to 100% width of the page.

--- cat-app/src/components/Navbar/Navbar.js
+++ cat-app/src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Nav, Navbar, Container } from 'react-bootstrap';
-import './Navbar.css';
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import "./Navbar.css";
 
 export default function Navigation() {
   return (
     <div>
-      <Navbar fixed='top' bg='dark' variant='dark' className="navbar">
+      <Navbar fixed="top" bg="dark" variant="dark" className="navbar">
+        <Navbar.Brand href="/">
+          <img alt="cat logo" src="images/cat.png" />
+          Cats-Fur-Ever!!!
+        </Navbar.Brand>
         {/* <Container>
           <Navbar.Toggle  />
           <Navbar.Collapse id='responsive-navbar-nav'>
@@ -16,12 +20,12 @@ export default function Navigation() {
            </Nav>
           </Navbar.Collapse>
         </Container> */}
-        
-        <Nav>
+
+        <Nav className="nav-links">
           <Nav.Link href="/form"> Form</Nav.Link>
           <Nav.Link href="/"> Home</Nav.Link>
         </Nav>
       </Navbar>
     </div>
-  )
+  );
 }
