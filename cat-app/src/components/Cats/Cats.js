629c8302468f420036e0b108e78d0374af91cb5f
samicap <lovestochasewindmills@gmail.com>
2021-06-08 13:56:44 -0400

    add favorite button to cats page

--- cat-app/src/components/Cats/Cats.js
+++ cat-app/src/components/Cats/Cats.js
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
-import './Cats.css';
+import React, { useState } from "react";
+import "./Cats.css";
 
-const axios = require('axios');
+const axios = require("axios");
 
 export default function Cats() {
   // setSate for the api response
-  const [ catPicture, setCatPicture ] = useState();
+  const [catPicture, setCatPicture] = useState();
 
   // Without the e as a parameter in helloClick the api is called without a click. called twice.
   // response.data[0].url is the cat
   function helloClick() {
-    axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:1, size:10}})
+    axios
+      .get("https://api.thecatapi.com/v1/images/search", {
+        params: { limit: 1, size: 10 },
+      })
       .then((response) => {
         const CatPictureURL = response.data[0].url;
         setCatPicture(CatPictureURL);
-        console.log("API Response: ", response.data[0])
-        console.log(CatPictureURL)
+        console.log("API Response: ", response.data[0]);
+        console.log(CatPictureURL);
       })
       .catch((error) => {
-        console.log("Error")
-      })
+        console.log("Error");
+      });
   }
 
   // need to create a function to call in the onClick
 
-  return(
+  return (
     <div className="random-cat-setup">
-      <h2 className="cat-picture-title">Hit the button to see another random cat picture!</h2>
-      <button onClick={helloClick} className="random-cat-search-button">Cat Search</button>
-      <img src={catPicture} className="cat-picture" alt="Cat"/>
+      <h2 className="cat-picture-title">
+        Hit the button to see another random cat picture!
+      </h2>
+      <span className="button-container">
+        <button onClick={helloClick} className="random-cat-search-button">
+          Cat Search
+        </button>
+        <button className="favorite-button">Favorite</button>
+      </span>
+      <img src={catPicture} className="cat-picture" alt="Cat" />
     </div>
   );
 }
