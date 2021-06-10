32309bcdc614ad737f1ffeb491a04fdb5b51fe3a
samicap <lovestochasewindmills@gmail.com>
2021-06-10 01:31:29 -0400

    add axios.get request to fetch user favorite pictures to dashboard

--- cat-app/src/components/Cats/Cats.js
+++ cat-app/src/components/Cats/Cats.js
@@ -52,27 +52,7 @@ export default function Cats() {
       console.log("Cat Picture Id: ", catPictureId)
   }
 
-  function fetchUserFavoritePictures() {
 
-    let query = {
-      sub_id: "User-123",
-      limit: 10
-    }
-
-    let headers = {
-      "x-api-key": process.env.REACT_APP_CAT_API_KEY,
-      "content-type": "application/json"
-  }
-
-    axios
-      .get("https://api.thecatapi.com/v1/favourites", query, {headers: headers})
-      .then((response) => {
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log("Error fetching user's favorite cat pictures: ", error)
-      })
-  }
 
   // need to create a function to call in the onClick
 
--- cat-app/src/components/Dashboard/Dashboard.js
+++ cat-app/src/components/Dashboard/Dashboard.js
@@ -2,6 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
+  
+  function fetchUserFavoritePictures() {
+
+    let query = {
+      sub_id: "User-123",
+      limit: 10
+    }
+
+    let headers = {
+      "x-api-key": process.env.REACT_APP_CAT_API_KEY,
+      "content-type": "application/json"
+  }
+
+    axios
+      .get("https://api.thecatapi.com/v1/favourites", query, {headers: headers})
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log("Error fetching user's favorite cat pictures: ", error)
+      })
+  } 
+
   return(
     <div>
       <h2>Dashboard</h2>
