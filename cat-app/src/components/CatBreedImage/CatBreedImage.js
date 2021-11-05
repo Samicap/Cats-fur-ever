9275bcfd1af2bcd85beb5d1cd152e46f83be850c
samicap <lovestochasewindmills@gmail.com>
2021-11-05 13:07:25 -0400

    image of cat breed now displayed on user search

--- cat-app/src/components/CatBreedImage/CatBreedImage.js
+++ cat-app/src/components/CatBreedImage/CatBreedImage.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function CatBreedImage (cat_id) {
-  const [ catImage, setCatImage] = useState();
+  // const defaultUrl = "https://cdn2.thecatapi.com/images/8ciqdpaO5.jpg"
+  const [ catPicture, setCatPicture] = useState("");
 
-  let image_id = cat_id.cat_id;
-
+  let cat_breed_id = cat_id.cat_id;
 
   let apiURL = `https://api.thecatapi.com/v1/images/search`;
   let apiKey = process.env.REACT_APP_CAT_API_KEY;
@@ -16,27 +16,52 @@ export default function CatBreedImage (cat_id) {
     }
   }
 
-  let params = {
-    params: {
-      limit: 1,
-      breed_id: `${image_id}`
-    }
-  }
+  useEffect(() => {
 
-  axios
-  .get(apiURL, params, headers,
-  )
-  .then((response) => {
-    console.log("catbreedImage", response.data[0].url);
-    // setCatImage(response.data[0].url)
-  })
-  .catch((error) => {
-    console.log("Error, ", error);
-  });
+    function requestCatPicture(cat_breed_id) {
+      axios
+      .get(apiURL, {params: {limit: 1, breed_id: `${cat_breed_id}`}}, headers,
+      )
+      .then((response) => {
+        console.log("axios call", response.data[0].url)
+        setCatPicture(response.data[0].url)
+      })
+      .catch((error) => {
+        console.log("Error, ", error);
+        return ""
+      });
+    }
+
+    requestCatPicture(cat_breed_id);
+    // function requestCatPicture(cat_breed_id) {
+    //   axios
+    //   .get(apiURL, {params: {limit: 1, breed_id: `${cat_breed_id}`}}, headers,
+    //   )
+    //   .then((response) => {
+    //     console.log(response.data[0].url)
+    //     return response.data[0].url
+    //   })
+    //   .catch((error) => {
+    //     console.log("Error, ", error);
+    //     return ""
+    //   });
+    // }
+  }, []);
+
+  // let params = {
+  //   params: {
+  //     limit: 1,
+  //     breed_id: `${cat_breed_id}`
+  //   }
+  // }
+
+  // let url = requestCatPicture(cat_breed_id)
+  //setCatPicture(url)
 
   return (
     <div>
     <p>hi</p>
+    <img src={catPicture} className="cat-pic" alt="Cat"></img>
     </div>
   )
 }
